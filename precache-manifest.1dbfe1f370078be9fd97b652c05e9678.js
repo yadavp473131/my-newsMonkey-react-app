a35578b14e67e78a9aa5b2a5ea6060ef32376916
yadavp473131 <yadavp8120@gmail.com>
2024-03-26 03:57:28 +0530

    Updates

---
+++ precache-manifest.1dbfe1f370078be9fd97b652c05e9678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e11dbf3da900118b829d499165a9163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "975e9fa9fc264bc08cb8",
+    "url": "/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "1e52f868adf6129263e7",
+    "url": "/static/js/2.ab7385c3.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "975e9fa9fc264bc08cb8",
+    "url": "/static/js/main.7918f210.chunk.js"
+  },
+  {
+    "revision": "3dfe5f0477b66a36e469",
+    "url": "/static/js/runtime~main.a1b9b0b5.js"
+  },
+  {
+    "revision": "bc6aef757e14b47e3e0f16a1b4874bf4",
+    "url": "/static/media/loading.gif.bc6aef75.gif"
+  }
+]);
