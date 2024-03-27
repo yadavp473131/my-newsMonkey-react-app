27d33a17e5513df47f492179207e57031fa5bdba
yadavp473131 <yadavp8120@gmail.com>
2024-03-27 11:35:35 +0530

    Updates

--- precache-manifest.365090c148f5ce9adc400e4fde66569e.js
+++ precache-manifest.365090c148f5ce9adc400e4fde66569e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8399adc58dcd6d3c3d6194aa7d15aecb",
+    "revision": "c4ce1ab097794618a1faeafe8ab7f8db",
     "url": "/my-newsMonkey-react-app/index.html"
   },
   {
-    "revision": "a5860291c265cbff4362",
+    "revision": "c44d5740478e00cdea50",
     "url": "/my-newsMonkey-react-app/static/css/main.be0c93d8.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-newsMonkey-react-app/static/js/3.7426b29a.chunk.js"
   },
   {
-    "revision": "a5860291c265cbff4362",
-    "url": "/my-newsMonkey-react-app/static/js/main.c1658e98.chunk.js"
+    "revision": "c44d5740478e00cdea50",
+    "url": "/my-newsMonkey-react-app/static/js/main.96953854.chunk.js"
   },
   {
     "revision": "dd2254b388ae3a2b0996",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-newsMonkey-react-app/precache-manifest.392f9a6c5237f990e1a9705d42cbc6d9.js"
+  "/my-newsMonkey-react-app/precache-manifest.365090c148f5ce9adc400e4fde66569e.js"
 );
 
 self.addEventListener('message', (event) => {
